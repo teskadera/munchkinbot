60952f144853d001ab23a9e28f14d3904a07dee6
Robert <teskadera@gmail.com>
2016-04-16 01:02:48 -0700

    Add commands to implement

--- tennu_plugins/munchkin.js
+++ tennu_plugins/munchkin.js
@@ -1,7 +1,59 @@
 var MunchkinPlugin = {
     init: function (client, imports) {
         return {
-            handlers: {},
+            handlers: {
+                '!start': function (command) {
+                    
+                },
+                '!startwith': function (command) {
+                    
+                },
+                '!kick': function (command) {
+                    
+                },
+                '!ban': function (command) {
+                    
+                },
+                '!join': function (command) {
+                    
+                },
+                '!quit': function (command) {
+                    
+                },
+                '!discardclass': function (command) {
+                    
+                },
+                '!discardrace': function (command) {
+                    
+                },
+                '!play': function (command) {
+                    
+                },
+                '!kickdownthedoor': function (command) {
+                    
+                },
+                '!loottheroom': function (command) {
+                    
+                },
+                '!lookfortrouble': function (command) {
+                    
+                },
+                '!endturn': function (command) {
+                    
+                },
+                '!slayit': function (command) {
+                    
+                },
+                '!runaway': function (command) {
+                    
+                },
+                '!bid': function (command) {
+                    
+                },
+                '!takebid': function (command) {
+                    
+                }
+            },
 
             help: {},
 
